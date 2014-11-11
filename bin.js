afb6e89450f48196fef6621264e3b958a135fd2f
Adam Rudd <adam.rudd@uqconnect.edu.au>
2014-11-11 17:06:34 +0100

    Add bin.js

---
+++ bin.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies
+ */
+var ebay = require('./')
+  , nomnom = require('nomnom');
+
+var opts = nomnom
+  .script('ebay-lookup')
+  .nocolors()
+  .option('id', {
+    abbr: 'i',
+    required: true,
+    help: 'Ebay api key'
+  })
+  .option('keywords', {
+    abbr: 'k',
+    required: true,
+    help: 'Keywords to search for'
+  })
+  .option('affiliate', {
+    abbr: 'a',
+    help: 'Affiliate id'
+  })
+  .parse();
+
+ebay({keywords: opts.keywords})
+  .id(opts.id)
+  .affiliate(opts.affiliate)
+  .done(function (err, res) {
+    if (err) throw err;
+    console.log(JSON.stringify(res));
+  });
